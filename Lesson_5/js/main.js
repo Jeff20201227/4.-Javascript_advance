dbbda0d44a157ae37ac1b0985fea9cecdedcc0e7
Evgeniy <Dgef2008@yandex.ru>
2021-08-09 14:17:36 +0300

    Lesson_5

--- Lesson_5/js/main.js
+++ Lesson_5/js/main.js
@@ -1,118 +1,76 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-class ProductsList {
-    constructor(container = '.products') {
-        this.container = container;
-        this.goods = [];//массив товаров
-        this.allProducts = [];//массив объектов
-        this._getProducts()
-            .then(data => { //data - объект js
-                this.goods = [...data];
-                this.render()
-            });
-    }
-
-    _getProducts() {
-        return fetch(`${API}/catalogData.json`)
-            .then(result => result.json())
-            .catch(error => {
-                console.log(error);
-            })
-    }
-    calcSum() {
-        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
-    }
-    render() {
-        const block = document.querySelector(this.container);
-        for (let product of this.goods) {
-            const productObj = new ProductItem(product);
-            this.allProducts.push(productObj);
-            block.insertAdjacentHTML('beforeend', productObj.render());
+const app = new Vue({
+    el: '#app',
+    data: {
+        userSearch: '',
+        showCart: false,
+        catalogUrl: '/catalogData.json',
+        cartUrl: '/getBasket.json',
+        cartItems: [],
+        filtered: [],
+        imgCart: 'https://via.placeholder.com/50x100',
+        products: [],
+        imgProduct: 'https://via.placeholder.com/200x150'
+    },
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => console.log(error))
+        },
+        addProduct(item){
+            this.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if(data.result === 1){
+                       let find = this.cartItems.find(el => el.id_product === item.id_product);
+                       if(find){
+                           find.quantity++;
+                       } else {
+                           const prod = Object.assign({quantity: 1}, item);
+                           this.cartItems.push(prod)
+                       }
+                    }
+                })
+        },
+        remove(item){
+            this.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if (data.result === 1) {
+                        if(item.quantity>1){
+                            item.quantity--;
+                        } else {
+                            this.cartItems.splice(this.cartItems.indexOf(item), 1);
+                        }
+                    }
+                })
+        },
+        filter(){
+            let regexp = new RegExp(this.userSearch, 'i');
+            this.filtered =  this.filtered.filter(el => regexp.test(el.product_name));
         }
-
-    }
-}
-
-
-class ProductItem {
-    constructor(product, img = 'http://dummyimage.com/200x150/00dd00') {
-        this.title = product.product_name;
-        this.price = product.price;
-        this.id = product.id_product;
-        this.img = img;
-    }
-    render() {
-        return `<div class="product-item" data-id="${this.id}">
-                <img src="${this.img}" alt="Some img">
-                <div class="desc">
-                    <h3>${this.title}</h3>
-                    <p>${this.price} $</p>
-                    <button class="buy-btn">Купить</button>
-                </div>
-            </div>`
-    }
-}
-
-let list = new ProductsList();
-
-
-class basket {
-    constructor(container = '.cart-block') {
-        this.container = container;
-        this.goods = [];
-       
-        this._clickBasket();
-        this._getBasketItem()
-            .then(data => { 
-                this.goods = [...data.contents];
-                this.render()
+    },
+    mounted(){
+        this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                for (let item of data.contents){
+                    this.cartItems.push(item);
+                }
             });
-    }
-
-
-    _getBasketItem() {
-        return fetch(`${API}/getBasket.json`)
-            .then(result => result.json())
-            .catch(error => {
-                console.log(error);
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let item of data){
+                    this.$data.products.push(item);
+                    this.$data.filtered.push(item);
+                }
+            });
+        this.getJson(`getProducts.json`)
+            .then(data => {
+                for(let item of data){
+                    this.products.push(item);
+                    this.filtered.push(item);
+                }
             })
     }
 
-    render() {
-        const block = document.querySelector(this.container);
-        for (let product of this.goods) {
-            const productObj = new BasketItem();
-            
-            block.insertAdjacentHTML('beforeend', productObj.render(product));
-        }
-
-    }
-
-    _clickBasket() {
-        document.querySelector(".btn-cart").addEventListener('click', () => {
-            document.querySelector(this.container).classList.toggle('invisible');
-        });
-    }
-}
-
-class BasketItem {
-  
-    render(product) {
-        return `<div class="cart-item" data-id="${product.id_product}">
-                <div class="product-bio">
-                <img src="${product.img}" alt="Фото товара">
-                <div class="product-desc">
-                <p class="product-title">${product.product_name}</p>
-                <p class="product-quantity">Quantity: ${product.quantity}</p>
-            <p class="product-single-price">$${product.price} each</p>
-            </div>
-            </div>
-            <div class="right-block">
-                <p class="product-price">$${product.quantity * product.price}</p>
-                <button class="del-btn" data-id="${product.id_product}">&times;</button>
-            </div>
-            </div>`
-    }
-}
-
-let bask = new basket();
+});
