52a30b68119e58767e0831e1b551ce8bba35b414
Evgeniy <Dgef2008@yandex.ru>
2021-08-21 18:00:33 +0300

    Lesson_8

--- Lesson_8/primer/webpack.config.js
+++ Lesson_8/primer/webpack.config.js
@@ -3,7 +3,11 @@ module.exports = {
     output: {
         path: 'D:\/Geekbrains_homework\/4_javascript_advance\/Lesson_8\/primer\/public\/js',
         filename: './components.js'
-    },
+  },
+    watchOptions: {
+            aggregateTimeout: 500,
+            poll: 1000 // порверяем изменения раз в секунду
+        },
     module: {
     rules: [
       { test: /\.svg$/, use: 'svg-inline-loader' },
